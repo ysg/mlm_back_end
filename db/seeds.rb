c8367fc431a0b75a7b679dd736cbbfc06fef3aeb
agustinh17 <agustinh17@gmail.com>
2012-06-01 01:59:24 -0300

    seed data issue #3

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.delete_all
+Identity.delete_all
 
 root = User.create
 root.name = 'root'
@@ -15,28 +16,29 @@ root.referer_id = 123456
 #root.password_confirmation = 'holanda'
 root.save
 
+id_andy = Identity.create :name => 'Andy', :email => 'andy@bpb.com', :password => 'holanda', :password_confirmation => 'holanda'
 
 andy = root.children.create
 andy.name = 'Andy'
 andy.email = 'andy@bpb.com'
-#andy.password = 'holanda'
-#andy.password_confirmation = 'holanda'
 andy.package = 1
 andy.referer_id = 654321
+andy.provider = 'identity'
+andy.uid = id_andy.id
 andy.save
+
+id_ysg = Identity.create :name => 'Jayandra', :email => 'jayandra@bpb.com', :password => 'holanda', :password_confirmation => 'holanda'
 
 ysg = andy.children.create 
 ysg.name = 'Jayandra'
 ysg.email = 'jayandra@bpb.com'
 ysg.package = 1
 ysg.referer_id = 123654
-#ysg.password = 'holanda'
-#ysg.password_confirmation = 'holanda'
+ysg.provider = 'identity'
+ysg.uid = id_ysg.id
 ysg.save
 
-ah = andy.children.create 
-#ah.password = 'holanda'
-#ah.password_confirmation = 'holanda'
+ah = andy.children.create
 ah.name = 'Agustin Hernandez'
 ah.email = 'agustin@bpb.com'
 ah.package = 2
@@ -48,16 +50,12 @@ juan.name = 'Juan Pequenio'
 juan.email = 'juan@bpb.com'
 juan.package = 1
 juan.referer_id = 666666
-#juan.password = 'holanda'
-#juan.password_confirmation = 'holanda'
 juan.save
 
 emi = ysg.children.create 
 emi.name = 'Emiliano Garcia', 
 emi.email = 'emiliano@bpb.com', 
 emi.package = 2
-#emi.password = 'holanda'
-#emi.password_confirmation = 'holanda'
 emi.referer_id = 999999
 emi.save
 
@@ -66,8 +64,5 @@ remer.name = 'Miguel Revetria'
 remer.email = 'remer@bpb.com'
 remer.package = 3
 remer.referer_id = 111111
-#remer.password = 'holanda'
-#remer.password_confirmation = 'holanda'
 remer.save
-#users = 
-#	User.create([{ name: 'Agustin', email: 'agustinh17@gmail.com', parent_id: root.id}, { name: 'Andy', email: 'andyjbas@gmail.com', parent_id: root.id} ])
+
