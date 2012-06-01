d92d95bd967208a24a3558f035a0cba4b592bad9
jayandra <root@jayandra-Latitude-D520.(none)>
2012-06-01 18:50:32 +0545

    -payment notifications

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -1,10 +1,14 @@
 class PaymentNotificationsController < ApplicationController
   protect_from_forgery :except => [:create]
 
+  def index
+    @payments = current_user.payment_notifications
+  end
+
   def create
-    puts"-------------------- payment_notification----------------------"
-    puts params.inspect
-    puts"-----------------------pn end----------------------------------"
+    #puts"-------------------- payment_notification----------------------"
+    #puts params.inspect
+    #puts"-----------------------pn end----------------------------------"
     PaymentNotification.create!(:params => params, :user_id => params[:custom], :status => params[:payment_status], :transaction_id => params[:txn_id])
     render :nothing => true
   end
