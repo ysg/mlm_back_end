12a3f61390704625fbd01c52b50f3a3ea502d2fe
jayandra <root@jayandra-Latitude-D520.(none)>
2012-06-01 12:35:47 +0545

    minor

--- app/assets/javascripts/accounts.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-      $(".network-modal-link").bind("ajax:success",
-               function(evt, data, status, xhr){
-                    //this assumes the action returns an HTML snippet
-                    $("#network-modal").html(data);
-       });
-});
