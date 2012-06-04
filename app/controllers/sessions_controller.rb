6d77ffe7695f7770f2e48b73378eb78471b0239b
jayandra <root@jayandra-Latitude-D520.(none)>
2012-06-04 12:28:03 +0545

    minor

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
    user = User.from_omniauth(env["omniauth.auth"],params)
    session[:user_id] = user.id
    #Redirect to paypal if from signup
-   if (/identit/.match(request.referer).present? && user.package!=3)
+   if (/identit/.match(request.referer).present? && params["package"]!="3")
      redirect_to user.paypal_url(my_account_url, payment_notifications_url, User::PACKAGE_COSTS[params["package"]]) and return
    end
    session["return_to"] = params[:return_to] if params[:return_to].present?
