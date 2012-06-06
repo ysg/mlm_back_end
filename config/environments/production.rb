40297b38c440479e7014be5b2e10535c7f889391
Andy Bas <andyjbas@gmail.com>
2012-06-05 23:07:04 -0700

    bug fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ MlmBackEnd::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => "mlmbe.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "bpb.herokuapp.com" }
 end
