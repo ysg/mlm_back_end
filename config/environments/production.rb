0fe327707fff86d39b6a742d0baa5c2aa0986045
agustinh17 <agustinh17@gmail.com>
2012-06-27 01:45:06 -0300

    prueba assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,17 +12,17 @@ MlmBackEnd::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = false
-
-  # Expands the lines which load the assets
-  config.assets.debug = true
-
+  config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
+
+  # Enable the asset pipeline
+  config.assets.enabled = false
+
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
