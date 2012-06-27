c00ad678b99bc4f563d277fc027da68e31d2f622
agustinh17 <agustinh17@gmail.com>
2012-06-27 01:30:01 -0300

    prueba assets

--- config/application.rb
+++ config/application.rb
@@ -71,7 +71,7 @@ config.action_mailer.default_url_options = {
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = true
+    config.assets.enabled = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
